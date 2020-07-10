1025f65b969bf6736605f498ac50f3bd4b28d80e
Mr. Robot <joselsondias@hotmail.com>
2020-07-10 21:45:43 +0100

    Style login screen

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import SignIn from "../views/AuthenticationFlow/SignIn";
+import Recover from "../views/AuthenticationFlow/Recover";
+import Request from "../views/AuthenticationFlow/Request";
 
 Vue.use(VueRouter);
 
@@ -8,23 +12,36 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
+
   {
     path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    name: "about",
+    component: About,
+  },
+
+  {
+    path: "/recover",
+    name: "recover",
+    component: Recover,
+  },
+  {
+    path: "/signin",
+    name: "signin",
+    component: SignIn,
+  },
+  {
+    path: "/request",
+    name: "request",
+    component: Request,
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
