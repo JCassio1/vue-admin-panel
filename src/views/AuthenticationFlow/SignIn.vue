b57d3d707c4be9f6523205ed479f33dda8589937
Mr. Robot <joselsondias@hotmail.com>
2020-07-12 02:04:31 +0100

    Add dark mode to project

--- src/views/AuthenticationFlow/SignIn.vue
+++ src/views/AuthenticationFlow/SignIn.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="container">
-    <div class="request">
-      Don't have an account with us yet? router
+  <div
+    class="container"
+    :class="{'light-background' : !isDarkMode, 'dark-background' : isDarkMode}"
+  >
+    <div class="request" :class="{'light-request' : isDarkMode, 'dark-request' : !isDarkMode}">
+      Don't have an account with us yet?
       <router-link to="/request">Open an account</router-link>
     </div>
     <div class="login">
@@ -9,12 +12,31 @@
         src="@/assets/codigo-de-barras-with-colorLetters.png"
         width="45%"
         height="45%"
+        v-show="isDarkMode"
       />
-      <h4>Sign into Código QR</h4>
-      <input type="email" placeholder="email" />
-      <input type="password" placeholder="password" />
+      <img
+        src="@/assets/codigo-de-barras-with-colorLetters-black.png"
+        width="45%"
+        height="45%"
+        v-show="!isDarkMode"
+      />
+      <h4 :class="{'light-text' : isDarkMode, 'dark-text' : !isDarkMode}">Sign into Código QR</h4>
+      <input
+        type="email"
+        placeholder="email"
+        :class="{'light-field' : isDarkMode, 'dark-field' : !isDarkMode}"
+      />
+      <input
+        type="password"
+        placeholder="password"
+        :class="{'light-field' : isDarkMode, 'dark-field' : !isDarkMode}"
+      />
       <button>Sign In</button>
-      <router-link to="/recover">Forgot my password </router-link>
+      <router-link
+        to="/recover"
+        :class="{'light-link' : isDarkMode, 'dark-link' : !isDarkMode}"
+      >Forgot my password</router-link>
+      <button @click="toggleDarkMode">Toggle</button>
     </div>
   </div>
 </template>
@@ -22,10 +44,58 @@
 <script>
 export default {
   name: "SignIn",
+  data() {
+    return {
+      isDarkMode: false
+    };
+  },
+  methods: {
+    toggleDarkMode() {
+      this.isDarkMode = !this.isDarkMode;
+      document.body.style.background = this.isDarkMode ? "#212c4f" : "#f0f3f5";
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+@import "@/Global-Styles/color.scss";
+@import "@/Global-Styles/typography.scss";
+
+.light-background {
+  background-color: $light-gray;
+}
+
+.dark-background {
+  background-color: $dark-blue;
+}
+
+.light-text {
+  color: $white;
+}
+
+.dark-text {
+  color: $black;
+}
+
+.light-field {
+  background: rgba(255, 255, 255, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.3);
+  }
+}
+
+.dark-field {
+  background: rgba(198, 208, 235, 0.2);
+  border: 1px solid rgba(0, 0, 0, 0.2);
+
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.3);
+  }
+}
+
 .container {
   display: flex;
   justify-content: center;
@@ -43,10 +113,21 @@ export default {
   top: 40px;
   right: 40px;
   color: rgba(255, 255, 255, 0.3);
+}
+
+.light-request {
+  color: rgba(255, 255, 255, 0.3);
 
   a {
     color: white;
-    text-decoration: none;
+  }
+}
+
+.dark-request {
+  color: rgba(0, 0, 0, 0.3);
+
+  a: {
+    color: black;
   }
 }
 
@@ -93,7 +174,15 @@ a {
   line-height: 25px;
   font-size: 16px;
   text-align: center;
-  color: rgba(255, 255, 255, 0.3);
+
   text-decoration: none;
+}
+
+.light-link {
+  color: rgba(255, 255, 255, 0.3);
+}
+
+.dark-link {
+  color: rgba(0, 0, 0, 0.3);
 }
 </style>
