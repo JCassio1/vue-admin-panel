7aa4765ca15e1e5fa53f5315fa0b5a7b898393f9
Mr. Robot <joselsondias@hotmail.com>
2020-07-12 13:25:29 +0100

    Add dark mode state transitions, local store and system preferences

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,16 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: "App",
+  mounted() {
+    const isDarkMode = this.$store.getters.isDarkMode;
+    document.body.style.background = isDarkMode ? "#212c4f" : "#f0f3f5";
+  }
+};
+</script>
+
 <style lang="scss">
 @import "@/Global-Styles/color.scss";
 @import "@/Global-Styles/typography.scss";
