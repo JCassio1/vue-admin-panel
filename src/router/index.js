0c9cf6585346a1c21eb9afe06c6f30992b7053ff
Mr. Robot <joselsondias@hotmail.com>
2020-07-12 15:22:43 +0100

    Add effects to buttons and animations to signIn page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import About from "../views/About.vue";
+import Team from "../views/Team.vue";
 import SignIn from "../views/AuthenticationFlow/SignIn";
 import Recover from "../views/AuthenticationFlow/Recover";
 import Request from "../views/AuthenticationFlow/Request";
@@ -16,9 +16,9 @@ const routes = [
   },
 
   {
-    path: "/about",
-    name: "about",
-    component: About,
+    path: "/team",
+    name: "team",
+    component: Team,
   },
 
   {
--- src/views/AuthenticationFlow/SignIn.vue
+++ src/views/AuthenticationFlow/SignIn.vue
@@ -1,12 +1,9 @@
 <template>
   <div
     class="container"
-    :class="{'light-background' : !isDarkMode, 'dark-background' : isDarkMode}"
+    :class="{ 'light-background': !isDarkMode, 'dark-background': isDarkMode }"
   >
-    <div class="request" :class="{'light-request' : isDarkMode, 'dark-request' : !isDarkMode}">
-      Don't have an account with us yet?
-      <router-link to="/request">Open an account</router-link>
-    </div>
+    <RequestAccount />
     <div class="login">
       <img
         src="@/assets/codigo-de-barras-with-colorLetters.png"
@@ -20,80 +17,50 @@
         height="45%"
         v-show="!isDarkMode"
       />
-      <h4 :class="{'light-text' : isDarkMode, 'dark-text' : !isDarkMode}">Sign into Código QR</h4>
+      <h4 :class="{ 'light-text': isDarkMode, 'dark-text': !isDarkMode }">
+        Sign into Código QR
+      </h4>
       <input
         type="email"
         placeholder="email"
-        :class="{'light-field' : isDarkMode, 'dark-field' : !isDarkMode}"
+        :class="{ 'light-field': isDarkMode, 'dark-field': !isDarkMode }"
       />
       <input
         type="password"
         placeholder="password"
-        :class="{'light-field' : isDarkMode, 'dark-field' : !isDarkMode}"
+        :class="{ 'light-field': isDarkMode, 'dark-field': !isDarkMode }"
       />
       <button>Sign In</button>
       <router-link
         to="/recover"
-        :class="{'light-link' : isDarkMode, 'dark-link' : !isDarkMode}"
-      >Forgot my password</router-link>
-      <button @click="toggleDarkMode">Toggle</button>
+        :class="{ 'light-link': isDarkMode, 'dark-link': !isDarkMode }"
+        >Forgot my password</router-link
+      >
+      <ThemeSwitch />
     </div>
   </div>
 </template>
 
 <script>
+import RequestAccount from "@/components/RequestAccount";
+import ThemeSwitch from "@/components/ThemeSwitch.vue";
 export default {
   name: "SignIn",
+  components: {
+    RequestAccount,
+    ThemeSwitch,
+  },
   computed: {
     isDarkMode() {
       return this.$store.getters.isDarkMode;
-    }
+    },
   },
-  methods: {
-    toggleDarkMode() {
-      this.$store.commit("toggleDarkMode");
-    }
-  }
 };
 </script>
 
 <style lang="scss" scoped>
 @import "@/Global-Styles/color.scss";
 @import "@/Global-Styles/typography.scss";
-
-.light-background {
-  background-color: $light-gray;
-}
-
-.dark-background {
-  background-color: $dark-blue;
-}
-
-.light-text {
-  color: $white;
-}
-
-.dark-text {
-  color: $black;
-}
-
-.light-field {
-  background: rgba(255, 255, 255, 0.2);
-  border: 1px solid rgba(255, 255, 255, 0.2);
-
-  &::placeholder {
-    color: rgba(255, 255, 255, 0.3);
-  }
-}
-
-.dark-field {
-  background: rgba(198, 208, 235, 0.2);
-  border: 1px solid rgba(0, 0, 0, 0.2);
-
-  &::placeholder {
-    color: rgba(0, 0, 0, 0.3);
-  }
-}
 
 .container {
   display: flex;
@@ -105,83 +72,5 @@ export default {
 
 .login {
   width: 400px;
-}
-
-.request {
-  position: absolute;
-  top: 40px;
-  right: 40px;
-  color: rgba(255, 255, 255, 0.3);
-}
-
-.light-request {
-  color: rgba(255, 255, 255, 0.3);
-
-  a {
-    color: white;
-  }
-}
-
-.dark-request {
-  color: rgba(0, 0, 0, 0.3);
-
-  a: {
-    color: black;
-  }
-}
-
-h4 {
-  margin: 0;
-  line-height: 34px;
-  font-size: 24px;
-  text-align: center;
-
-  color: #ffffff;
-}
-
-input {
-  background: rgba(255, 255, 255, 0.2);
-  border: 1px solid rgba(255, 255, 255, 0.2);
-  box-sizing: border-box;
-  border-radius: 4px;
-  height: 60px;
-  width: 100%;
-  font-size: 20px;
-  color: white;
-  padding-left: 20px;
-  margin-top: 20px;
-
-  &::placeholder {
-    color: rgba(255, 255, 255, 0.3);
-  }
-}
-
-button {
-  background: #56ccf2;
-  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
-  border-radius: 4px;
-  border: none;
-  height: 60px;
-  width: 100%;
-  font-size: 20px;
-  color: white;
-  margin-top: 20px;
-  margin-bottom: 40px;
-}
-
-a {
-  line-height: 25px;
-  font-size: 16px;
-  text-align: center;
-
-  text-decoration: none;
-}
-
-.light-link {
-  color: rgba(255, 255, 255, 0.3);
-}
-
-.dark-link {
-  color: rgba(0, 0, 0, 0.3);
 }
 </style>
